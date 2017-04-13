d1d1ecf1efa05227b740895fa5af668ae4664da0
amatsuo <matsuoakitaka@gmail.com>
2017-04-13 11:18:44 +0100

    Added a function xml_to_speechdf

---
+++ R/xml_to_speechdf.R
@@ -0,0 +1,23 @@
+#' Convert Kaigiroku XML to well-forated data frame
+#'
+#' @param xml_out an xml object returned from kokkai kaigiroku API
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#'
+xml_to_speechdf <- function(xml_out){
+  out_data <- NULL
+  meetings_node <- getNodeSet(xml_out, "//meetingRecord")
+  for(i in 1:length(meetings_node)){
+    meeting_node <- meetings_node[[i]]
+    meeting_list <- xmlToList(meeting_node, simplify = TRUE)
+    meeting_list <- meeting_list[which(names(meeting_list) != "speechRecord")]
+    meeting_info <- data.frame(meeting_list)
+    speech_df <- xmlToDataFrame(node = getNodeSet(meeting_node, "speechRecord"))
+    meeting_df <- cbind(meeting_info, speech_df)
+    out_data <- rbind(out_data, meeting_df)
+  }
+  return(out_data)
+}
