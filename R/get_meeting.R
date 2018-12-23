caa47a8d43c2f31304113d9519eaa104302505f0
amatsuo <matsuoakitaka@gmail.com>
2018-12-23 21:38:16 +0900

    change to strategy

--- R/get_meeting.R
+++ R/get_meeting.R
@@ -171,7 +171,9 @@ file_download <- function(url, quiet = FALSE){
   tmp_file <- tempfile()
   counter <- 0
   while(!file.exists(tmp_file)) {
-    tryCatch(withTimeout(download.file(url, tmp_file, quiet = quiet), timeout = 45),
+    if(couter <= 5) to <- 45
+    else to <- 300
+    tryCatch(withTimeout(download.file(url, tmp_file, quiet = quiet), timeout = to),
              TimeoutException = function(ex) {
                counter <<- counter + 1
                if(file.exists(tmp_file)) file.remove(tmp_file)
